901d25eaa7e08718df00a59041b6cbc337dda28c
Ritesh Kumar <kumarritesh14062000@gmail.com>
2023-10-04 15:23:07 +0530

    Increased pagination default number

--- src/utils/pagination.js
+++ src/utils/pagination.js
@@ -1,5 +1,5 @@
 exports.getPagination = (page, size) => {
-  const limit = size ? +size : 3;
+  const limit = size ? +size : 24;
   const offset = page ? (page - 1) * limit : 0;
 
   return { limit, offset };
